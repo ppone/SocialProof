d6d9d707f8c8e6fb17b48fd9e95d3b87bf90850b
Parag Patel <parag.patel.one@gmail.com>
2011-12-11 21:31:57 -0500

    First Social Proof Commit

---
+++ SocialProof/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialProof::Application.config.secret_token = '1b3454b20057f37dce0dd9f90a1478b2fdb3326cb8bd1634d7a0f21d57cd0be30b637f3806a84bd8b1caef05a4c42df8ce09d73b97bc6bae88010953b90070a2'
